0e8ba2b81ed9d32c99644c0bfb31072518fea9ce
Daniele Trovato <plasticsoup@yahoo.co.uk>
2020-01-15 18:13:56 +0000

    Add users names in the index.erb. Each name link to a page for that user. Each page has details. Add layout. Add method 'count_interests' in the helpers.

--- main.rb
+++ main.rb
@@ -2,3 +2,36 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'tilt/erubis'
 require 'pry'
+
+before do
+  @users = YAML.load_file("users.yaml")
+
+end
+
+helpers do
+  def count_interests
+    interests = 0
+    @users.each do |user, details|
+      interests += @users[user][:interests].size
+    end
+
+    interests
+  end
+end
+
+get "/sandbox" do
+  @users.inspect
+end
+
+get "/" do
+  @users = @users
+  erb(:index)
+end
+
+get "/users/:name" do
+  @name = params[:name]
+  @email = @users[@name.to_sym][:email]
+  @interests = @users[@name.to_sym][:interests].join(', ')
+
+  erb(:show)
+end
